0282e344cc28851fe757db7b9f2b39b9f9bdc483
Haden <haden.cain@gmail.com>
2026-05-30 11:10:19 -0400

    fix: move electron to devDependencies, add author, 256x256 installer icon

--- scripts/create-icons.js
+++ scripts/create-icons.js
@@ -2,8 +2,8 @@ const { PNG } = require('pngjs');
 const fs = require('fs');
 const path = require('path');
 
-function writeSolidPNG(r, g, b, outPath) {
-  const png = new PNG({ width: 16, height: 16, filterType: -1 });
+function writeSolidPNG(r, g, b, size, outPath) {
+  const png = new PNG({ width: size, height: size, filterType: -1 });
   for (let i = 0; i < png.data.length; i += 4) {
     png.data[i] = r;
     png.data[i + 1] = g;
@@ -17,8 +17,12 @@ function writeSolidPNG(r, g, b, outPath) {
 const assetsDir = path.join(__dirname, '..', 'assets');
 fs.mkdirSync(assetsDir, { recursive: true });
 
-writeSolidPNG(34, 197, 94,  path.join(assetsDir, 'icon-green.png'));
-writeSolidPNG(234, 179, 8,  path.join(assetsDir, 'icon-yellow.png'));
-writeSolidPNG(239, 68, 68,  path.join(assetsDir, 'icon-red.png'));
+// 16x16 tray icons
+writeSolidPNG(34, 197, 94,  16, path.join(assetsDir, 'icon-green.png'));
+writeSolidPNG(234, 179, 8,  16, path.join(assetsDir, 'icon-yellow.png'));
+writeSolidPNG(239, 68, 68,  16, path.join(assetsDir, 'icon-red.png'));
 
-console.log('Done. Replace with proper 16x16 icons before distributing.');
+// 256x256 installer icon
+writeSolidPNG(34, 197, 94, 256, path.join(assetsDir, 'icon-app.png'));
+
+console.log('Done. Replace placeholder icons in assets/ before distributing.');
